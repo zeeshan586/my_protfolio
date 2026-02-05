0603a5afcb035049b2a03d95a6c8429dda10e1d3
zeeshan <zeeshan@codebtech.com>
2026-02-05 15:33:20 +0500

    update work

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+
+} from "react-icons/di";
+import {
+  SiPytorch,
+  SiFirebase,
+  SiNextdotjs,
+  SiDart,
+  SiFlutter,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDart />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlutter />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiPytorch />
+      </Col> */}
+    </Row>
+  );
+}
+
+export default Techstack;
