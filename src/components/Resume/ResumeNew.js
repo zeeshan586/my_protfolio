232b45987ba0a617707a4a1b5ae20a3e63e1afd0
zeeshan <zeeshan@codebtech.com>
2026-03-25 13:16:47 +0500

    update work

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/zeeshan_sr_frontEnd_developer.pdf";
+import pdf from "../../Assets/../Assets/ZeeshanResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 
